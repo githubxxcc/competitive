61b8c1dfa0b3d7b7b8d719533ec9f373a0532842
githubxxcc <xuchen727@hotmail.com>
2017-12-02 11:33:16 -0800

    abc

---
+++ kattis/abc/sol.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+using namespace std;
+
+
+string a;
+
+int main() {
+    vector<int> v(3, 0);
+    cin >> v[0] >> v[1] >> v[2];
+
+    sort(v.begin(), v.end());
+    
+    cin >> a;
+    printf("%d %d %d", v[a[0]-'A'] , v[a[1]-'A'] , v[a[2]-'A'] );
+}
