2d920df3e03b89c582c9aca4cb02ff38b763ca03
githubxxcc <xuchen727@hotmail.com>
2017-12-03 19:05:27 -0800

    temperatur

---
+++ kattis/temperature/sol.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+using namespace std;
+
+double n, m;
+
+int main() 
+{
+    cin >> n >> m;
+    
+    if(n == 0 && m == 1)
+        cout << "ALL GOOD" << endl;
+    else if(m == 1 && n != 0)
+        cout << "IMPOSSIBLE" << endl;
+    else 
+        cout << fixed << setprecision(10) <<  n / (1.0-m) << endl;
+}
