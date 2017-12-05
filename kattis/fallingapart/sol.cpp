62e3efd5959b94e31bd87680a5f41c12cd6285b4
githubxxcc <xuchen727@hotmail.com>
2017-12-05 09:02:16 -0800

    fallingapart

---
+++ kattis/fallingapart/sol.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+
+int n, _n, i, A, B, a[100] = {};
+
+int main ()
+{
+    cin >> n;
+    _n = n, i = 0;
+
+    while(_n--) {
+        cin >> a[i++];     
+    }
+
+    vector<int> v(a, a+ n);
+    sort(RALL(v));
+
+    for(i = 0; i<n;i++) {
+        if(i % 2 ==0 ) 
+            A+=v[i];
+        else 
+            B += v[i];
+    }
+
+    cout << A << " " << B;
+}
