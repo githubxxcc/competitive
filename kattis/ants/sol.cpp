090b0110500e014d33392655271babb18d288e7a
githubxxcc <xuchen727@hotmail.com>
2017-12-06 10:32:44 -0800

    ants

---
+++ kattis/ants/sol.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+
+int main ()
+{
+    int mmin, mmax, L, x, n, TC;
+    cin >> TC;
+    while(TC--) {
+        cin >> L >> n;
+        mmin = 0, mmax = 0;
+        while(n--) {
+            cin >> x;
+
+            mmin = max(mmin, min(x, L-x));
+            mmax = max(mmax, max(x, L-x));
+        }
+
+        cout << mmin << " " << mmax << endl;
+    }
+}
