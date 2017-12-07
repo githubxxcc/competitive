03f2c5d214347f3cbf830bc80600d12120ba6285
githubxxcc <xuchen727@hotmail.com>
2017-12-07 08:43:23 -0800

    Beat the Spread

---
+++ kattis/beatspread/sol.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+#define ALL(x) x.begin(), x.end()
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+int main()
+{
+
+    int TC, sum, diff, large, small;
+    cin >> TC;
+
+    while(TC--) {
+        cin >> sum >> diff;
+        
+        large = (sum+diff)/2;
+        small = large - diff;
+
+        if(large >= 0 && small >= 0 && large+small == sum) 
+            cout << large << " " << small << endl;
+        else 
+            cout << "impossible" << endl;
+    }
+}
