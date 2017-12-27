dd4595eb740ae0afa8d4e05742ddd910246f96c1
githubxxcc <xuchen727@hotmail.com>
2017-12-28 00:03:37 +0800

    putovanje

---
+++ kattis/putovanje/sol.cpp
@@ -0,0 +1,62 @@
+
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+#define ALL(x) x.begin(), x.end()
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+typedef vector<int> vi;
+
+
+int main()
+{
+    int N, C, arr[1001] = {}, best, sum, ans = 0;
+
+    cin >> N >> C;
+
+    REP(i, N) {
+        cin >> arr[i];
+    }
+
+    REP(i, N) {
+        best = sum = 0;
+        for(int j = i; j < N; j++) {
+           if(sum + arr[j] <= C) 
+               best++, sum+=arr[j];
+        }
+        
+        ans = max(ans, best);
+    }
+
+    cout << ans << endl;
+}
