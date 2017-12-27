861ca6c836ce8bd7828de0a69494d6518ea18320
githubxxcc <xuchen727@hotmail.com>
2017-12-27 17:35:54 +0800

    Carousel Rides

---
+++ kattis/carousel/sol.cpp
@@ -0,0 +1,60 @@
+
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+#define ALL(x) x.begin(), x.end()
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+typedef vector<int> vi;
+
+
+int main()
+{
+    int n, m, a, b, i, x, y;
+
+    while(cin >> n >> m, (n || m)) {
+        y = 0, x = 1;
+        REP(i, n) {
+            cin >> b >> a;
+            if(b <= m && (a * y < b * x || (a * y == b * x && b > y))) {
+                x= a, y= b;
+            }
+        }
+
+        if(y == 0) {
+            cout << "No suitable tickets offered" << endl;
+        } else {
+            printf("Buy %d tickets for $%d\n", y, x);
+        }
+    }
+}
