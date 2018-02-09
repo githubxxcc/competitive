95ba56fcb645eaffc9dca2636730bda3774c092a
githubxxcc <xuchen727@hotmail.com>
2018-02-09 09:57:14 +0800

    bitsequalizer

---
+++ kattis/bitsequalizer/sol.cpp
@@ -0,0 +1,76 @@
+
+#include <algorithm>
+#include <unordered_map>
+#include <cctype>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <sstream>
+
+#define FileIn(file) freopen(file".inp", "r", stdin)
+#define FileOut(file) freopen(file".out", "w", stdout)
+#define FOR(i, a, b) for (int i=a; i<=b; i++)
+#define REP(i, n) for (int i=0; i<n; i++)
+#define Fill(ar, val) memset(ar, val, sizeof(ar))
+#define PI 3.1415926535897932385
+#define uint64 unsigned long long
+#define int64 long long
+#define all(ar) ar.begin(), ar.end()
+#define pb push_back
+#define bit(n) (1<<(n))
+#define Last(i) ( i & -i )
+#define INF 500000000
+#define maxN 101
+#define RALL(x) x.rbegin(), x.rend()
+#define ALL(x) x.begin(), x.end()
+#define D_PSTL(msg, stl) cout << msg<< endl; for(auto x: stl) cout << x << " "; \
+    cout << endl << endl;
+
+int d8[9] = {-1, 0, 1, 0, -1, 1, 1, -1, -1};
+int d4[5] = {-1, 0, 1, 0 , -1};
+using namespace std;
+
+typedef vector<int> vi;
+
+int main() {
+    string A, B;
+    int cnt, zeros,q_zeros, ones,C, q, i, size, diff, tc = 1;
+    
+    cin >> C;
+
+    while(C--) {
+        cin >> A >> B;
+        size = A.length();
+
+        q_zeros = q = cnt = zeros = ones = 0;
+
+        REP(i, size) {
+            if(A[i] == '?'){
+                q++;
+                if(B[i] == '1') q_zeros++;
+            } else {
+                diff = A[i]-B[i];
+                if(diff > 0) {
+                    ones++;
+                } else if(diff < 0){
+                    zeros++;
+                } 
+            }
+        }
+        if(ones > (zeros+q_zeros)) {
+            cnt = -1;
+        } else {
+            cnt = q+ ones;
+            if(zeros > ones) cnt += zeros-ones;
+        }
+
+        printf("Case %d: %d\n", tc++, cnt);
+    }
+}
